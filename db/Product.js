9983e48d9895ef079b19c3013c6d411fd8b0ce5d
Atif1727 <ghoghaatif2002@gmail.com>
2023-01-03 13:18:41 +0530

    Updated

---
+++ db/Product.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose');
+
+const ProductSchema=new mongoose.Schema({
+    userId:String,
+    name:String,
+    price:String,
+    category:String,
+    company:String
+});
+
+
+module.exports=mongoose.model('products', ProductSchema);
--- index.js
+++ index.js
@@ -1,16 +1,74 @@
 const express = require('express');
-const cors=require('cors');
-require("./db/config");
-const User=require("./db/User");
+const cors = require('cors');
+require('./db/config');
+const User = require('./db/User');
+const Product = require('./db/Product');
 const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-app.post('/register',async (req,res)=>{
-    let user=new User(req.body);
-    let result = await user.save();
+app.post('/register', async (req, res) => {
+    let user = new User(req.body)
+    let result = await user.save()
+    result = result.toObject();
+    delete result.password;
+    res.send(result)
+});
+
+app.post('/login', async (req, res) => {
+    //select and -password means it cannot show as password of the given field
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select('-password');
+        if (user) {
+            res.send(user);
+        }
+        else {
+            res.send({ result: 'not users found' });
+        }
+    }
+});
+
+
+app.post('/add-product', async (req, res) => {
+    let product = new Product(req.body);
+    let result = await product.save();
+    res.send(result);
+
+});
+
+app.get('/product-list', async (req, res) => {
+    let products = await Product.find();
+    if (products.length > 0) {
+        res.send(products);
+    } else {
+        res.send({ result: 'not products found' });
+    }
+});
+
+
+app.delete('/product/:id', async (req, res) => {
+    const result = await Product.deleteOne({ _id: req.params.id });
     res.send(result);
 });
 
-app.listen(5000, () => console.log(`app listening on port 5000!`))
+app.get('/product/:id', async (req, res) => {
+    let result = await Product.findOne({ _id: req.params.id });
+    if (result) {
+        res.send(result);
+    } else {
+        res.send({ result: 'Result not found' });
+    }
+});
+
+app.put('/product/:id', async (req, res) => {
+    let result = await Product.updateOne(
+        { _id: req.params.id },
+        {
+            $set: req.body
+        }
+    )
+    res.send(result);
+});
+
+app.listen(5000, () => console.log(`app listening on port 5000!`));
